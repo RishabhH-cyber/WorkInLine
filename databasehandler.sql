ebbb8ee9a925b69f04afe87b8a62f8b9fbc31751
nikita2995 <khandelwalnikita02@gmail.com>
2021-10-31 19:16:11 +0530

    Update databasehandler.sql

--- databasehandler.sql
+++ databasehandler.sql
@@ -6,3 +6,16 @@ FirstName varchar(255),
 Address varchar(255),
 City varchar(255)
 );
+CREATE TABLE Orders (
+    OrderID int NOT NULL,
+    OrderNumber int NOT NULL,
+    PersonID int,
+    PRIMARY KEY (OrderID),
+    CONSTRAINT FK_PersonOrder FOREIGN KEY (PersonID)
+    REFERENCES Persons(PersonID)
+);
+ALTER TABLE Orders
+ADD FOREIGN KEY (PersonID) REFERENCES Persons(PersonID);
+ALTER TABLE Orders
+ADD CONSTRAINT FK_PersonOrder
+FOREIGN KEY (PersonID) REFERENCES Persons(PersonID);
